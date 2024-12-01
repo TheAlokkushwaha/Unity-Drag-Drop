97a61800293689b579cd4154a7d3f8a9ff7773c5
thealokkushwaha <103347563+thealokkushwaha@users.noreply.github.com>
2024-12-01 12:33:53 +0530

    Initial commit

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Transform originalParent;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(originalParent.parent);
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(
+                transform as RectTransform, eventData.position, eventData.pressEventCamera, out Vector3 worldPosition))
+        {
+            transform.position = worldPosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+
+        if (transform.parent == originalParent.parent)
+        {
+            transform.SetParent(originalParent);
+        }
+    }
+}
